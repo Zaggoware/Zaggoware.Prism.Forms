b3600fadf756edf8183bf6c5e45d68911dd090c7
Zaggo0 <patrickvd_linden@hotmail.com>
2020-09-27 04:32:40 +0200

    Add some basic converters

---
+++ src/Zaggoware.Prism.Forms/Converters/BooleanInverter.cs
@@ -0,0 +1,25 @@
+﻿namespace Zaggoware.Prism.Forms.Converters
+{
+    using System;
+    using System.Globalization;
+
+    using Xamarin.Forms;
+    
+    public class BooleanInverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+
+            return value;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
---
+++ src/Zaggoware.Prism.Forms/Converters/HasNoValueConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace Zaggoware.Prism.Forms.Converters
+{
+    using System;
+    using System.Globalization;
+
+    using Xamarin.Forms;
+    
+    public class HasNoValueConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue)
+            {
+                return string.IsNullOrEmpty(stringValue);
+            }
+
+            return value == null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
